a3093e4139d19ce7fca1812badf6ff124fb7e2d7
Diaz-Lunar <0216114@students.dadeschools.net>
2019-02-15 14:17:02 -0500

    did some css

--- main.js
+++ main.js
@@ -11,15 +11,37 @@ let car = {
         this.mileage += 10;
         alert(`New Mileage: ${this.mileage}`);
     },
-}
+
+    driveAroundUSA(){
+        alert(`Old Mileage: ${this.mileage}`);
+        this.mileage += 7000;
+        alert(`New Mileage: ${this.mileage}`);
+        alert('Car needs a tuneup!');
+        this.isWorking = false;
+    },
+
+    getTuneUp(){
+        alert('Car is fixed and ready to go!');
+        this.isWorking = true;
+    },
+
+    honk(){
+        alert("Honk! Honk!");
+    }
+};
 
 // FUNCTIONS
 // Logs all of our car's current stats to the console
-/*
-function reWriteStats(){
+reWriteStats=()=>{
+    console.log(`Make: ${car.make}`);
+    console.log(`Model: ${car.model}`);
+    console.log(`Color: ${car.color}`);
+    console.log(`Mileage: ${car.mileage}`);
+    console.log(`IsWorking: ${car.isWorking}`);
+    console.log('-----------------------------');
 
 }
-*/
+   
 
 // MAIN PROCESS
 // Captures keyboard input. Depending on the letter pressed it will "call" (execute) different functions.
@@ -32,4 +54,19 @@ document.onkeyup = (event) => {
         reWriteStats();
     }
 
+    if (letter === "r") {
+        car.driveAroundUSA();
+        reWriteStats();
+    }
+
+    if (letter === "t") {
+        car.getTuneUp();
+        reWriteStats();
+    }
+
+    if (letter === "h") {
+        car.honk();
+        reWriteStats();
+    }
+    
 }
